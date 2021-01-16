fa897b6a0ffe487ce7351756e1a71ee5ecf83c66
Yoram Zilbershats <jerichozil@gmail.com>
2021-01-16 19:42:41 +0200

    added pagination to products table

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,13 +1,12 @@
 class CategoriesController < ApplicationController
 	def index
-		# @categories = Category.all
 		@categories = Category.order(name: :asc)
 	end
 
 	def show
 		@categories = Category.order(name: :asc)
     	@category = Category.find(params[:id])
-    	@products = Product.where(:category_id => params[:id]).order(name: (params[:sort])? (params[:sort]) : (:asc))
+    	@products = Product.where(:category_id => params[:id]).order(name: (params[:sort])? (params[:sort]) : (:asc)).paginate(page: params[:page], per_page: 5)
 	end
 
 end
